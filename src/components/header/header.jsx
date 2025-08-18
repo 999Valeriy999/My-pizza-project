2edc3044f0caa0ed5411836f294ad22a6f36b55b
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2025-08-18 16:08:55 +0300

    upd

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,28 @@
-import logo from '../header/logo/image_1.svg';
+import React from 'react';
+import logo from './logo/image_1.svg';
 
 function Header() {
-return(
- <div className='Header_logo'> 
-   <img width="36" src=
-   {logo} alt='image_1' />
-     <h1>React Pizza</h1> 
-    
- </div>
- );
+  return (
+  
+    <div className="header-container">
+      <div className="Header_logo"> 
+        <img width="36" src={logo} alt="Логотип пиццы" /> {/* alt текст описываемый */}
+        <button>Заказать</button> {/* текст для кнопки */}
+        <h1>React Pizza</h1>
+        <p>Super pizza</p>
+      </div>
+
+      <div className="header_card">
+        {/*
+          <Link> из react-router-dom для навигации вместо <a>, т.к. используется react-router-dom.
+          <a class="button_card" href='./card'>
+        */}
+        <Link className="button_card" href='./card'>
+          <span>0 грн.</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default Header;
